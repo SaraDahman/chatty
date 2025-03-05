8f19eb004590d867495e7f5bb2342eb10504c168
unknown <saritadh74@gmail.com>
2025-03-05 03:17:16 +0200

    created the login and register forms

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,54 @@
+import { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+
 const Login = () => {
-  return <>login</>;
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (e) => {
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
+  return (
+    <Form
+      noValidate
+      validated={validated}
+      onSubmit={handleSubmit}
+      style={{ width: '50%', margin: '0 auto', paddingTop: '4%' }}
+    >
+      <h2
+        className='mb-5 text-warning'
+        style={{ fontWeight: '700', fontSize: '2.5rem' }}
+      >
+        Log in to Your Account
+      </h2>
+
+      <Form.Group controlId='validationCustom02' className='mb-4'>
+        <Form.Label>Email</Form.Label>
+        <Form.Control required type='email' placeholder='john@gmail.com' />
+        <Form.Control.Feedback type='invalid'>
+          Please enter a valid email.
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group controlId='validationCustom03' className='mb-5'>
+        <Form.Label>Password</Form.Label>
+        <Form.Control required type='password' placeholder='password' />
+        <Form.Control.Feedback type='invalid'>
+          Please enter a strong password.
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Button variant='primary' type='submit'>
+        Submit
+      </Button>
+    </Form>
+  );
 };
 
 export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,61 @@
+import { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+
 const Register = () => {
-  return <>register</>;
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (e) => {
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
+  return (
+    <Form
+      noValidate
+      validated={validated}
+      onSubmit={handleSubmit}
+      style={{ width: '50%', margin: '0 auto', paddingTop: '4%' }}
+    >
+      <h2
+        className='mb-5 text-warning'
+        style={{ fontWeight: '700', fontSize: '2.5rem' }}
+      >
+        Create Your Account
+      </h2>
+      <Form.Group controlId='validationCustom01' className='mb-4'>
+        <Form.Label>Name</Form.Label>
+        <Form.Control required type='text' placeholder='John' variant='dark' />
+        <Form.Control.Feedback type='invalid'>
+          Please enter a Name.
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group controlId='validationCustom02' className='mb-4'>
+        <Form.Label>Email</Form.Label>
+        <Form.Control required type='email' placeholder='john@gmail.com' />
+        <Form.Control.Feedback type='invalid'>
+          Please enter a valid email.
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group controlId='validationCustom03' className='mb-5'>
+        <Form.Label>Password</Form.Label>
+        <Form.Control required type='password' placeholder='password' />
+        <Form.Control.Feedback type='invalid'>
+          Please enter a strong password.
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Button variant='primary' type='submit'>
+        Submit
+      </Button>
+    </Form>
+  );
 };
 
 export default Register;
