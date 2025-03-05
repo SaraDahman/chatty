cab3615ca3329d014d5849013d957157096632f3
unknown <saritadh74@gmail.com>
2025-03-05 03:18:30 +0200

    installed react bootstrap and bootstrap icons, created the navbar and app router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,25 @@
+// import './index.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Chat from './pages/Chat';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import NavBar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Chat />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='*' element={<Navigate to='/' />} />
-      </Routes>
+      <NavBar />
+      <Container>
+        <Routes>
+          <Route path='/' element={<Chat />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='*' element={<Navigate to='/' />} />
+        </Routes>
+      </Container>
     </>
   );
 }
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import { Container, Navbar, Stack, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <Navbar className='bg-body-tertiary px-5 mb-4' data-bs-theme='dark'>
+      <Navbar.Brand style={{ fontSize: '2.3rem', fontWeight: '700' }}>
+        <Link to='/' className='text-decoration-none text-white'>
+          <i
+            className='bi bi-chat-heart-fill'
+            style={{ marginRight: '0.7rem' }}
+          ></i>
+          Chatty
+        </Link>
+      </Navbar.Brand>
+      <Navbar.Collapse className='justify-content-end'>
+        {/* <Stack direction='horizontal' gap={4}>
+          <Navbar.Text className='text-white'>
+            Signed in as <b className='text-warning'>Sara Dahman</b>
+          </Navbar.Text>
+          <Button variant='outline-light' size='sm'>
+            Log Out
+          </Button>
+        </Stack> */}
+
+        <Stack direction='horizontal' gap={2}>
+          <Button as={Link} to='/register' variant='outline-light'>
+            Register
+          </Button>
+          <Button as={Link} to='/login' variant='outline-light'>
+            Log in
+          </Button>
+        </Stack>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default NavBar;
