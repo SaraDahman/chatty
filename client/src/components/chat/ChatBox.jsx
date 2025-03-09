161475a9d0baa8d2e21489036666ffc74b62d0e8
unknown <saritadh74@gmail.com>
2025-03-09 05:17:59 +0200

    modified the page overall layout a bit

---
+++ client/src/components/chat/ChatBox.jsx
@@ -0,0 +1,96 @@
+import { Button, Spinner, Stack } from 'react-bootstrap';
+import moment from 'moment';
+import InputEmoji from 'react-input-emoji';
+import { useState } from 'react';
+
+const ChatBox = ({
+  currentChat,
+  recipient,
+  chatMessages,
+  user,
+  messageMutation,
+}) => {
+  const [textMessage, setTextMessage] = useState('');
+
+  if (chatMessages.isLoading) {
+    return (
+      <div style={{ textAlign: 'center', width: '100%' }}>
+        <Spinner animation='border' variant='primary' size='xl' />
+      </div>
+    );
+  }
+
+  if (!chatMessages.data) {
+    return (
+      <p style={{ textAlign: 'center', width: '100%', fontWeight: 'bold' }}>
+        No chat was selected yet
+      </p>
+    );
+  }
+
+  const handleCreateMessage = () => {
+    messageMutation.mutate({
+      senderId: user._id,
+      chatId: currentChat._id,
+      text: textMessage,
+    });
+
+    setTextMessage('');
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter' && textMessage.trim()) {
+      event.preventDefault();
+      handleCreateMessage();
+    }
+  };
+
+  return (
+    <Stack gap={4} className='chat-box'>
+      <div className='chat-header'>
+        <b>Chat with {recipient.name}</b>
+      </div>
+      <Stack gap={3} className='messages'>
+        {chatMessages.data &&
+          chatMessages.data.map((e) => {
+            return (
+              <Stack
+                key={e._id}
+                className={`${
+                  e.senderId == user._id
+                    ? 'message self align-self-end flex-grow-0'
+                    : 'message align-self-start flex-grow-0'
+                }`}
+              >
+                <span className='mb-1'>{e.text}</span>
+                <span className='message-footer'>
+                  {moment(e.createdAt).calendar()}
+                </span>
+              </Stack>
+            );
+          })}
+      </Stack>
+
+      <Stack direction='horizontal' gap={3} className='chat-input flex-grow-0'>
+        <InputEmoji
+          value={textMessage}
+          onChange={setTextMessage}
+          onKeyDown={handleKeyDown}
+          fontFamily='nunito'
+          borderColor='rgba(72, 112, 223, 0.2)'
+        />
+        <Button
+          className='send-btn p-0'
+          style={{ borderRadius: '50%' }}
+          variant='primary'
+          onClick={handleCreateMessage}
+          disabled={!textMessage.trim()}
+        >
+          <i className='bi bi-send-fill'></i>
+        </Button>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default ChatBox;
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,41 +1,61 @@
 import { useContext } from 'react';
 import ChatContext from '../context/ChatContext';
-import { Container, Stack } from 'react-bootstrap';
+import { Container, Stack, Row, Col } from 'react-bootstrap';
+import AuthContext from '../context/AuthContext';
 import UserCard from '../components/chat/UserCard';
 import PotentialChats from '../components/chat/PotentialChats';
-import AuthContext from '../context/AuthContext';
+import ChatBox from '../components/chat/ChatBox';
 
 const Chat = () => {
   const { user } = useContext(AuthContext);
   const {
     fetchChatsResponse: { data, isLoading },
     updateCurrentChat,
+    currentChat,
+    recipient,
+    chatMessages,
+    messageMutation,
+    onlineUsers,
   } = useContext(ChatContext);
 
   return (
     <Container>
-      <PotentialChats user={user} />
-
-      <h3 className='mb-4'>Your Chats </h3>
       {isLoading ? (
         <p>Loading ..</p>
-      ) : !data?.length ? (
-        <p>You haven't started a conversation yet</p>
       ) : (
-        <Stack direction='horizontal' className='align-items-start' gap={4}>
-          <Stack className='messages-box flex-grow-0 pe-3' gap={3}>
-            {data.map((chat, i) => (
-              <div key={i}>
-                <UserCard
-                  chat={chat}
-                  user={user}
-                  updateCurrentChat={updateCurrentChat}
-                />
-              </div>
-            ))}
-          </Stack>
-          <div>chat box</div>
-        </Stack>
+        <Row>
+          <Col sm={4}>
+            <div>
+              <PotentialChats user={user} />
+              <h3 className='mb-4'>Your Chats </h3>
+              {!data?.length ? (
+                <p>You haven't started a conversation yet</p>
+              ) : (
+                <Stack className='messages-box flex-grow-0 pe-3' gap={3}>
+                  {data.map((chat, i) => (
+                    <div key={i}>
+                      <UserCard
+                        chat={chat}
+                        user={user}
+                        updateCurrentChat={updateCurrentChat}
+                        onlineUsers={onlineUsers}
+                      />
+                    </div>
+                  ))}
+                </Stack>
+              )}
+            </div>
+          </Col>
+          <Col sm={8}>
+            <ChatBox
+              currentChat={currentChat}
+              recipient={recipient}
+              chatMessages={chatMessages}
+              user={user}
+              messageMutation={messageMutation}
+            />
+          </Col>
+        </Row>
       )}
     </Container>
   );
